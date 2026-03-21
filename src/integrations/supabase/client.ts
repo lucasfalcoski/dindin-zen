23f257b878074c6333d4aa6f955cb89e28c1b7f5
lucasfalcoski <lucas.falcoski@gmail.com>
2026-03-21 15:34:25 -0300

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,12 +2,11 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const SUPABASE_URL = "https://xktkvrctjsonrscocuvn.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhrdGt2cmN0anNvbnJzY29jdXZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQxMDU5NTIsImV4cCI6MjA4OTY4MTk1Mn0.wrcTS5OO8rOWOO6k8DMg2WtPyoYweWLPMygtJnhLfsQ";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
-
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
   auth: {
     storage: localStorage,
